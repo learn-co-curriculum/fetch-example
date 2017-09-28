ca182e26dbf04efc622ce3e88b19fafd8d60a6a7
Rachel Salois <rachel.salois@gmail.com>
2017-09-28 16:14:00 -0400

    lecture notes

---
+++ Character.js
@@ -0,0 +1,8 @@
+class Character {
+  constructor(attributes){
+    this.name = name
+  }
+  render(){
+    `<li>${this.name}</li>`
+  }
+}
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 fetch("https://swapi.co/api/people")
   .then(res => res.json())
-  .then(res => console.log(res))
+  .then(res => appendCharacter(res))
 
 console.log("loading...")
 
 function appendCharacter(data){
-  data.forEach((char) => {
-    debugger
+  // console.log(data)
+  data["results"].forEach((char) => {
+    console.log(char)
   })
 }
+// 1. someone clicks submit
+// 2. callback gets called from event listener
+// 3. get form entered data
+// 4. make post request to API with data
+// 5. OPTIMISTIC RENDERING: create div and rendering to the dom whether it saved or not
+// 6. PESSIMISTIC RENDER: wait for the response to come back, and rerender based on response
+
+// POST request
+// no longer default get request
+// method is POST
+// sending data within the body
+// through the route itself
+// '/users/1/edit'
